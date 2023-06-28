8f11051c0e7923df73ced8464c68c406e1740528
Tiago Ferreira <tiagoferreira977@gmail.com>
2023-06-28 20:01:32 +0100

    Service layer, component registration

---
+++ src/main/java/ferreira/trc/course/services/UserService.java
@@ -0,0 +1,26 @@
+package ferreira.trc.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ferreira.trc.course.entities.User;
+import ferreira.trc.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+    
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findBuId(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
