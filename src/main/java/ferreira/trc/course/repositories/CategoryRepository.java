cd74f69b88cbeec1c26d367ca71e20308de24f3c
Tiago Ferreira <tiagoferreira977@gmail.com>
2023-07-05 21:43:01 +0100

    Category

---
+++ src/main/java/ferreira/trc/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package ferreira.trc.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import ferreira.trc.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+    
+}
---
+++ src/main/java/ferreira/trc/course/services/CategoryService.java
@@ -0,0 +1,27 @@
+package ferreira.trc.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ferreira.trc.course.entities.Category;
+import ferreira.trc.course.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+    
+    @Autowired
+    private CategoryRepository repository;
+
+    public List<Category> findAll() {
+        return repository.findAll();
+    }
+
+    public Category findBuId(Long id) {
+        Optional<Category> obj = repository.findById(id);
+        return obj.get();
+    }
+}
